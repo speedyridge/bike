fe1a32aab9b638817b02193416652829b54bc000
speedyridge <speedyridge@gmail.com>
2026-08-26 17:57:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
 
+const CACHE_NAME = 'bikename-v1';
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
